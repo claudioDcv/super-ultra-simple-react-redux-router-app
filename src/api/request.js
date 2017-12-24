700d647716be3b69b1c1f78e5dbe1ad9d4b282e5
Claudio Rojas Rodriguez <claudio.dcv@gmail.com>
2017-12-24 17:25:59 -0300

    fix request

--- src/api/request.js
+++ src/api/request.js
@@ -4,9 +4,35 @@ import { loadLoginSuccess, signOff } from '../actions/auth'
 
 const MAX_TIME_SECOND_UPDATE_TOKEN = 1800 // 30 minutos
 
+
+const get = (obj, xhr, resolve, reject) => {
+  if (obj.headers) {
+      Object.keys(obj.headers).forEach(key => {
+          try {
+            xhr.setRequestHeader(key, obj.headers[key]);
+          } catch (err) {
+
+          }
+      });
+  }
+  xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+          resolve(JSON.parse(xhr.response));
+      } else {
+          if (xhr.status === 401) {
+            store.dispatch(signOff(xhr.status))
+          }
+          reject(JSON.parse(xhr.response))
+      }
+  };
+  xhr.onerror = () => reject(xhr.statusText);
+  xhr.send(obj.body);
+}
+
 let request = obj => {
     return new Promise((resolve, reject) => {
         let xhr = new XMLHttpRequest();
+        let isSend = false
         xhr.open(obj.method || "GET", obj.url);
 
         obj.headers = {
@@ -18,6 +44,7 @@ let request = obj => {
         if (obj.authorization) {
           const state = store.getState()
           let token = state.auth.get.token
+          obj.headers['Authorization'] = `JWT ${token}`
           const date = state.auth.dateLastToken
           if (date) {
             const lastTime = (new Date().getTime() - date) / 1000
@@ -26,29 +53,21 @@ let request = obj => {
                 token: token,
               }).then(d => {
                 store.dispatch(loadLoginSuccess(d, store.dispatch))
+
+                let xhrNew = new XMLHttpRequest();
+                xhrNew.open(obj.method || "GET", obj.url);
+                if (!isSend) {
+                  get(obj, xhrNew, resolve, reject)
+                  isSend = true
+                }
               })
             }
           }
-
-          obj.headers['Authorization'] = `JWT ${token}`
         }
-        if (obj.headers) {
-            Object.keys(obj.headers).forEach(key => {
-                xhr.setRequestHeader(key, obj.headers[key]);
-            });
+        if (!isSend) {
+          get(obj, xhr, resolve, reject)
+          isSend = true
         }
-        xhr.onload = () => {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(JSON.parse(xhr.response));
-            } else {
-                if (xhr.status === 401) {
-                  store.dispatch(signOff(xhr.status))
-                }
-                reject(JSON.parse(xhr.response))
-            }
-        };
-        xhr.onerror = () => reject(xhr.statusText);
-        xhr.send(obj.body);
     });
 };
 
